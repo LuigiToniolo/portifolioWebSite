fb7f574b759de5d5e7c7383a332b33e3b2a6bd18
LuigiToniolo <luigibtoniolo@gmail.com>
2022-10-06 22:11:43 -0300

    finalizando botao portifolio

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -7,21 +7,27 @@ export const Newsletter = () => {
         <Row>
           <Col lg={12} md={6} xl={5}>
             <h3>
-              Click in the button<br></br>and Download my Resume :)
+              Click in the button<br></br>and Download my Resume!
             </h3>
           </Col>
 
           <Col md={6} xl={7}>
-            
-            <div class="text-box">
+
+            <div class="portifolio">
               <a
-                class="btn btn-white btn-animate"
                 href="Luigi_Toniolo_Resume.pdf"
                 download="Luigi Toniolo Resume.pdf"
               >
-                Download
+                <button
+                  type="submit"
+                  href="Luigi_Toniolo_Resume.pdf"
+                  download="Luigi Toniolo Resume.pdf"
+                >
+                  <span>Download my Portifolio ;)</span>
+                </button>
               </a>
             </div>
+
           </Col>
         </Row>
       </div>
