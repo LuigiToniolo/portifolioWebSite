d9ef92a62fb3b00ae377a6bc6caa3454fe4449ad
LuigiToniolo <luigibtoniolo@gmail.com>
2022-10-10 18:03:31 -0300

    substituindo logo instagram pelo github

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,6 +6,7 @@ import logo from '../assets/img/logoteste.png';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
+import github from '../assets/img/GitHubIcon.png';
 import { HashLink } from 'react-router-hash-link';
 import {
   BrowserRouter as Router
@@ -52,9 +53,9 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="https://www.linkedin.com/in/luigi-toniolo/"><img src={navIcon1} alt="" /></a>
-                <a href="https://www.facebook.com/luigi.benetitoniolo/"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="https://www.linkedin.com/in/luigi-toniolo/"><img src={navIcon1} alt="linkedin icon" /></a>
+                <a href="https://www.facebook.com/luigi.benetitoniolo/"><img src={navIcon2} alt="facebook icon" /></a>
+                <a href="https://github.com/LuigiToniolo"><img src={github} alt="github icon" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>{i18n.t('banner.connect')}</span></button>
