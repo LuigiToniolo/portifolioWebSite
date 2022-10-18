c915a81e14e766acdc8a31b52466c8019738cbbc
LuigiToniolo <luigibtoniolo@gmail.com>
2022-10-18 19:34:38 -0300

    iniciando desenvolvimento das modais

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,6 +7,7 @@ import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+import { ModalProject } from './Modal.js';
 
 export const Projects = () => {
 
@@ -52,6 +53,9 @@ export const Projects = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>{i18n.t('projects.title')}</h2>
+
+                <ModalProject/>
+
                 <p>{i18n.t('projects.subtitle')}</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
