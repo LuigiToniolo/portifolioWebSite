fa71cef886e7b538058577f850d6046345c8bff3
LuigiToniolo <luigibtoniolo@gmail.com>
2022-11-04 15:48:34 -0300

    finalizando modal exemplo toniolo joias

---
+++ src/components/Modal/data.js
@@ -0,0 +1,24 @@
+import imacMock from "../../assets/img/imacMock.png";
+
+export const data = {
+    modal1: {
+      title: "Conteúdo modal 1",
+      content: "Aqui eu coloco o conteúdo da modal",
+    },
+    modal2: {
+      title: "E-commerce WordPress",
+      imagem: imacMock,
+      titleContent: "Toniolo Joias Web Site",
+      content: "Eu desenvolvi e liderei um projeto inteiro para essa empresa. O projeto consistia em desenvolver do zero um e-commerce de joias usando o WordPress e Woocommerce (Plugin). Como foi realizado em WordPress, foi usado muito, para o desenvolvimento desse e-commerce, todo meu conhecimento de HTML5 e CSS3 e até mesmo SCSS.  Além disso, o site inteiro foi otimizado para Search Engine Optimization (SEO). Após o término do desenvolvimento desse projeto, nós decidimos investir em ads como as propagandas do google e facebook para aumentar a visualização do nosso site. Com isso, nós atingimos mais de R$10k em vendas num período menor que 1 mês. Além de investir no tráfego pago, nós investimos também no tráfego orgânico, como dito anteriormente, melhorando o SEO de todo website.",
+      techTitle: "Tecnologias usadas",
+      techList: {
+        one: "WordPress",
+        two: "Html / CSS",
+        three: "WooCommerce",
+      }
+    },
+    modal3: {
+      title: "Conteúdo modal 3",
+      content: "Aqui eu coloco o conteúdo da modal 3",
+    },
+  };
--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,37 +1,55 @@
-import React from 'react';
+import React from "react";
 import "./modal.css";
+import { data } from "./data";
+// import imacMock from "../../assets/img/imacMock.png";
 
 export const ModalContent1 = () => {
-
-    return (
-        <div className='conteudo-modal'>
-            <h2 className='titulo-modal'>Conteúdo modal 1</h2>
-            
-        </div>
-
-    );
-}
+  return (
+    <div className="conteudo-modal">
+      <h2 className="titulo-modal">{data.modal1.title}</h2>
+      <p>{data.modal1.content}</p>
+    </div>
+  );
+};
 
 export const ModalContent2 = () => {
-
-    return (
-        <div className='conteudo-modal'>
-            <h2 className='titulo-modal'>Coteúdo modal 2</h2>
-            
+  return (
+    <div className="conteudo-modal">
+      <h2 className="titulo-modal">{data.modal2.title}</h2>
+      <div className="project-content">
+        <div className="project-image">
+          <img src={data.modal2.imagem} />
         </div>
 
-    );
-}
+        <div className="project-info">
+          <h3>{data.modal2.titleContent}</h3>
+          <p>{data.modal2.content}</p>
+          <h4>{data.modal2.techTitle}</h4>
+          <div className="tech-list">
+            <ul className="col">
+              <li>{data.modal2.techList.one}</li>
+              <li>{data.modal2.techList.four}</li>
+            </ul>
+            <ul className="col">
+              <li>{data.modal2.techList.two}</li>
+              <li>{data.modal2.techList.five}</li>
+            </ul>
+            <ul className="col">
+              <li>{data.modal2.techList.three}</li>
+              <li>{data.modal2.techList.six}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export const ModalContent3 = () => {
-
-    return (
-        <div className='conteudo-modal'>
-            <h2 className='titulo-modal'>Coteúdo modal 3</h2>
-            
-        </div>
-
-    );
-}
-
-
+  return (
+    <div className="conteudo-modal">
+      <h2 className="titulo-modal">{data.modal3.title}</h2>
+      <p>{data.modal3.content}</p>
+    </div>
+  );
+};
